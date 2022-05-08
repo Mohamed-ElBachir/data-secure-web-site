82fb29935ba41f8c1b66866675dfabd9107bae28
Achour Mohamed El Bachir <achour.med.bachir@gmail.com>
2022-05-08 05:30:37 +0100

    prepared the navBar

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './heroStyles.css'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="container">
+        <div className="content">
+          <h1>Data</h1>
+          <h1 className='blue'>Secured</h1>
+          <h1>Protection</h1>
+        </div>
+        <div><button>Learn More</button></div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
